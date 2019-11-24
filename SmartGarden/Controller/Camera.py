5f06c6c151c737bd14a17ba26a62ba9e9ef8a5e0
nuttchai <43024164+nuttchai@users.noreply.github.com>
2019-11-24 20:36:15 +0700

    Add files via upload

---
+++ SmartGarden/Controller/Camera.py
@@ -0,0 +1,12 @@
+from picamera import PiCamera
+from time import sleep
+from datetime import *
+import asyncio
+
+async def camera():
+    today = datetime.now()
+    camera = PiCamera()
+    camera.start_preview()
+    camera.capture('/home/pi/Desktop/smartGarden/SmartGarden/Pictures/'+str(today)+'.jpg')
+    camera.stop_preview()
+    return today
